519d55aff0458af5ee48ed1651c1ca75c3cc3632
gjlander <garrett.jlander@gmail.com>
2023-10-20 12:59:39 +0200

    got a json of users too, without wishlist data

--- migration/migrate.mjs
+++ migration/migrate.mjs
@@ -1,12 +1,17 @@
 import fs from "fs/promises";
 
 try {
-    const data = await fetch();
+    const data = await fetch(
+        " https://cdn.contentful.com/spaces/<spaceID>/environments/master/entries?access_token=<accessToken>&content_type=user"
+    );
     //this is where the URL with our access tokens was, but we don't need to run this function again
     const parsedData = await data.json();
-    const trimData = parsedData.items.map((item) => item.fields);
+    const trimData = parsedData.items.map((item) => {
+        const { wishlist, ...rest } = item.fields;
+        return rest;
+    });
     await fs.writeFile(
-        "contentfulAlbumsData.json",
+        "contentfulUserData.json",
         JSON.stringify(trimData),
         "utf8"
     );
